e7e374f2b51d0d2ff6bb8165688faf6bcf25afa1
Mikołaj Lechtański <mikolec@gmail.com>
2020-06-26 16:36:38 +0200

    Run Multiple Promises with Promise.all() and Promise.race()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import { fetchWithTimeout } from "./services";
+import { fetchMovies, fetchBooks } from "./services";
+// import { fetchWithTimeout } from "./services";
 const movies = require("./data/movies.json");
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
-}
+// export function fetchMovies() {
+//   const resolveFunction = () => movies;
+//   return fetchWithTimeout(1000).then(resolveFunction);
+// }
+// var moviePromise = fetchMovies();
+// moviePromise.then((results) => {
+//   console.log(results);
+// });
 
-var moviePromise = fetchMovies();
-moviePromise.then((results) => {
-  console.log(results);
+function getBooksAndMovies() {
+  return Promise.all([fetchMovies(), fetchBooks()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch((error) => console.log("Error fetching books and movies", error));
+}
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+  console.log("getBooksAndMoviesPromise", results);
+});
+
+function getBooksOrMovies() {
+  return Promise.race([fetchMovies(), fetchBooks()])
+    .then((results) => results)
+    .catch((error) => console.log("Error waiting for the promise race", error));
+}
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then((results) => {
+  console.log("getBooksOrMoviesPromise", results);
 });
