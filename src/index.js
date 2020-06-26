21e94eaa75faac3c6a9158d9159639b6223e0138
Mikołaj Lechtański <mikolec@gmail.com>
2020-06-26 17:43:42 +0200

    Create Async versions for Promise.all() and Promise.race()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchMovies, fetchBooks } from "./services";
+import { fetchMovies, fetchBooks, asyncFetchBooks, asyncFetchMovies } from "./services";
 // import { fetchWithTimeout } from "./services";
 const movies = require("./data/movies.json");
 
@@ -32,4 +32,35 @@ function getBooksOrMovies() {
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then((results) => {
   console.log("getBooksOrMoviesPromise", results);
+});
+
+async function getBooksAndMoviesAsync() {
+  try {
+    const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
+    return { books, movies };
+  } catch (error) {
+    console.log("Error fetching books and movies", error);
+  }
+}
+
+async function getBooksOrMoviesAsync() {
+  try {
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+    return values;
+  } catch (error) {
+    console.error("Error waiting for the promise race", error);
+  }
+}
+
+getBooksAndMoviesAsync().then((results) => {
+  console.log("movies and books", {
+    movies: results.movies,
+    books: results.books,
+  });
+});
+
+getBooksOrMoviesAsync().then((results) => {
+  console.log("movies OR books", {
+    results,
+  });
 });
